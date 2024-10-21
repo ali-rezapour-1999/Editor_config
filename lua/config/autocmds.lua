ef7196fcdf022e3d72a65872d9b419f90281a681
ali-rezapour-1999 <ali.programer.1999@gmail.com>
2024-10-21 22:11:39 +0330

    add lazy git

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
+})
